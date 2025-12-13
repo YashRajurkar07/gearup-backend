2ac0fdf349fa8781c694f46791dfc2839340b55d
Yash Rajurkar <yashrajurkar04@gmail.com>
2025-12-13 22:58:57 +0530

    Initial Commit

---
+++ src/main/java/com/gearup/GearupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gearup/GearupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
